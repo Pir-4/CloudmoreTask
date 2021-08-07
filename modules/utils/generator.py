58f4a12ab521b4858eb242932bb4df0490803f10
valentin <valentin-42@mail.ru>
2021-08-07 18:29:03 +0300

    Create methods with negative cases

--- modules/utils/generator.py
+++ modules/utils/generator.py
@@ -1,6 +1,7 @@
 import random
 import sys
 import string
+from copy import copy, deepcopy
 
 from modules import tests_constants as TC
 
@@ -18,16 +19,47 @@ def get_bodies_modify(number=5, excluded_params=None):
     bodies = []
     for _ in range(number):
         first_body = create_body(excluded_params)
-        second_body = create_body(excluded_params)
-        second_body["username"] = first_body["username"]
+        second_body = first_body
         while second_body == first_body:
             second_body = create_body(excluded_params)
-            second_body["username"] = first_body["first_body"]
+            second_body["username"] = first_body["username"]
         bodies.append((first_body, second_body))
     return bodies
 
 
-def create_body(excluded_params=None):
+def get_negative_post_bodies():
+    """"""
+    bodies = [create_body()]#body with id
+    param_list = copy(TC.PARAM_LIST)
+    param_list.remove("id")
+    for param_name in param_list:
+        bodies.append(create_body(excluded_params=["id", param_name]))
+    for _ in range(5):
+        bodies.append(create_body(excluded_params=["id"], is_bad_format=True))
+    return bodies
+
+
+def get_negative_put_bodies():
+    """"""
+    body = create_body(excluded_params=["id"])
+    modify_body = deepcopy(body)
+    modify_body.update({"id": random.randint(0, sys.maxsize)})
+    bodies = [(body, modify_body)]
+
+    param_list = copy(TC.PARAM_LIST)
+    param_list.remove("id")
+    for param_name in param_list:
+        body = create_body(excluded_params=["id"])
+        modify_body = create_body(excluded_params=["id", param_name])
+        bodies.append((body, modify_body))
+    for _ in range(5):
+        body = create_body(excluded_params=["id"])
+        modify_body = create_body(excluded_params=["id"], is_bad_format=True)
+        bodies.append((body, modify_body))
+    return bodies
+
+
+def create_body(excluded_params=None, is_bad_format=False):
     """"""
     excluded_params = excluded_params or []
     excluded_params = excluded_params if isinstance(excluded_params, list) else [excluded_params]
@@ -40,7 +72,10 @@ def create_body(excluded_params=None):
     for body_param in TC.PARAM_LIST:
         if body_param in excluded_params:
             continue
-        body[body_param] = gen_funcs[TC.PARAM_TYPES[body_param]]()
+        param_format = TC.PARAM_TYPES[body_param]
+        if is_bad_format:
+            param_format = random.choice([item for item in TC.PARAM_TYPES.values() if item != param_format])
+        body[body_param] = gen_funcs[param_format]()
     return body
 
 
