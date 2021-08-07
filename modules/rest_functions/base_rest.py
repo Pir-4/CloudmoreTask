00ad7fe375613830d1c01874485e5e062f216c65
valentin <valentin-42@mail.ru>
2021-08-07 19:26:11 +0300

    Add doc strings to rest classes

--- modules/rest_functions/base_rest.py
+++ modules/rest_functions/base_rest.py
@@ -3,19 +3,38 @@ from modules import tests_constants as TC
 
 
 class BaseRestApi:
-    """"""
+    """
+    Provide base Rest methods
+    """
     _requestsCalls = {"GET": requests.get,
                       "POST": requests.post,
                       "PUT": requests.put,
                       "DEL": requests.delete}
 
-    def __init__(self, baseUrl=TC.BASE_URL):
-        """"""
-        self._baseUrl = baseUrl
+    def __init__(self, base_url=TC.BASE_URL):
+        """ Init class
+
+        :param base_url: url of an information system
+        :type base_url: str
+        :returns: None
+        """
+        self._baseUrl = base_url
         self._header = {}
 
     def request(self, req_type, rest_obj, obj_id, params=None):
-        """"""
+        """ Send request
+
+        :param req_type: request type: Get, Post, Delete, Put
+        :type req_type: str
+        :param rest_obj: name or rest object
+        :type rest_obj: str
+        :param obj_id: id of rest object or command
+        :type obj_id: str
+        :param params: request body
+        :type params: dict
+        :return: main parts of response: status_code, reason, text, success
+        :rtype: dict
+        """
         request_url = self._get_url(self._baseUrl, rest_obj, obj_id)
         params = params or {}
 
@@ -27,11 +46,22 @@ class BaseRestApi:
         return result
 
     def update_header(self, header):
-        """"""
+        """ Update headers
+
+        :param header: additional header parameters
+        :type header: dict
+        :returns: None
+        """
         self._header.update(header)
 
     def _get_url(self, *args):
-        """"""
+        """ Get full request url
+
+        :param args: parts of url for joining
+        :type: list
+        :return: full request url
+        :rtype: str
+        """
         if self._baseUrl not in args:
             args.insert(0, self._baseUrl)
         return "/".join(args)
--- modules/rest_functions/user_rest.py
+++ modules/rest_functions/user_rest.py
@@ -2,37 +2,67 @@ from modules.rest_functions.base_rest import BaseRestApi
 from modules import tests_constants as TC
 import json
 
+
 class UserRest:
-    """"""
+    """
+    Provide Rest API for User rest object
+    """
 
     @staticmethod
     def create(users):
-        """"""
+        """ Create user or users
+
+        :param users: list of users bodies
+        :type users: list
+        :return: status code
+        :type: int
+        """
         base_rest = BaseRestApi()
-        base_rest.update_header({"W-Token": "Ilovemyboss"})
         users = users if isinstance(users, list) else [users]
         result = base_rest.request("POST", TC.REST_OBJ_USER, TC.USER_CREATE_LIST, params=users)
         return result["status_code"]
 
     @staticmethod
-    def get(user_name, expectedError=False):
-        """"""
+    def get(user_name, expected_error=False):
+        """ Get user entry
+
+        :param user_name: user name
+        :type user_name: str
+        :param expected_error:
+        :type expected_error: true if request is unsuccess
+        :return: status code and user body
+        :rtype: tuple
+        """
         base_rest = BaseRestApi()
         result = base_rest.request("GET", TC.REST_OBJ_USER, user_name)
-        if expectedError:
+        if expected_error:
             return result["status_code"], result
         return result["status_code"], json.loads(result["text"])
 
     @staticmethod
     def modify(user_name, body):
-        """"""
+        """ Modify user entry via Put
+
+        :param user_name: user name
+        :type user_name: str
+        :param body: new body for modifying
+        :type body: dict
+        :return: status code
+        :rtype: int
+        """
         base_rest = BaseRestApi()
         result = base_rest.request("PUT", TC.REST_OBJ_USER, user_name, body)
         return result["status_code"]
 
     @staticmethod
     def delete(user_name):
-        """"""
+        """ Delete user entry
+
+        :param user_name: user name
+        :type user_name: str
+        :return: status code
+        :rtype: int
+        """
         base_rest = BaseRestApi()
         result = base_rest.request("DEL", TC.REST_OBJ_USER, user_name)
         return result["status_code"]
