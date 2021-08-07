5b66b0419d74d16eabad7f9566c9b7eb3ed18bd5
valentin <valentin-42@mail.ru>
2021-08-07 17:39:42 +0300

    Add parametrize

--- tests/test_rest_user.py
+++ tests/test_rest_user.py
@@ -1,54 +1,43 @@
 import pytest
 from http import HTTPStatus
 from modules.rest_functions.user_rest import UserRest
+from modules.utils import generator
 
 
-def test_positive_create():
+@pytest.mark.parametrize('input_body', generator.get_bodies(excluded_params=["id"]))
+def test_positive_create(input_body):
     """"""
-    user_body = {"username": "Testuser", "firstName": "firstName",
-                 "lastName": "lastName", "email": "tmp@email.com", "password": "Password123!",
-                 "phone": "156456", "userStatus": 0
-                 }
-    assert UserRest.create(user_body) == HTTPStatus.OK, "The POST returned unexpected status code"
-    status, user = UserRest.get(user_body["username"])
+    assert UserRest.create(input_body) == HTTPStatus.OK, "The POST returned unexpected status code"
+    status, user = UserRest.get(input_body["username"])
     assert status == HTTPStatus.OK, "The GET returned unexpected status code"
     user_id = user.pop("id")
     assert isinstance(user_id, int) and user_id, "User id isn't int or equal 0"
-    assert user == user_body, "The expected body doesn't equal actual"
+    assert not user.get("password") and user.get("password") == input_body["password"], "GET return user password"
+    assert user == input_body, "The expected body doesn't equal actual"
 
 
-def test_positive_delete():
+@pytest.mark.parametrize('input_body', generator.get_bodies(excluded_params=["id"]))
+def test_positive_delete(input_body):
     """"""
-    user_body = {"username": "Testuser", "firstName": "firstName",
-                 "lastName": "lastName", "email": "tmp@email.com", "password": "Password123!",
-                 "phone": "156456", "userStatus": 0
-                 }
-    assert UserRest.create(user_body) == HTTPStatus.OK, "The POST returned unexpected status code"
-    status, _ = UserRest.get(user_body["username"])
+    assert UserRest.create(input_body) == HTTPStatus.OK, "The POST returned unexpected status code"
+    status, _ = UserRest.get(input_body["username"])
     assert status == HTTPStatus.OK, "The GET returned unexpected status code"
-    assert UserRest.delete(user_body["username"]), "Un success user deleting"
-    status, _ = UserRest.get(user_body["username"], expectedError=True)
+    assert UserRest.delete(input_body["username"]), "Un success user deleting"
+    status, _ = UserRest.get(input_body["username"], expectedError=True)
     assert status == HTTPStatus.NOT_FOUND, "The DELETE returned unexpected status code"
 
 
-def test_positive_modify():
+@pytest.mark.parametrize('input_body, modify_body', generator.get_bodies_modify(excluded_params=["id"]))
+def test_positive_modify(input_body, modify_body):
     """"""
-    user_body_1 = {"username": "Testuser", "firstName": "firstName",
-                 "lastName": "lastName", "email": "tmp@email.com", "password": "Password123!",
-                 "phone": "156456", "userStatus": 0
-                 }
-
-    user_body_2 = {"username": "Testuser", "firstName": "firstName2",
-                   "lastName": "lastName", "email": "tmp@email.com", "password": "Password123!",
-                   "phone": "156456", "userStatus": 0
-                   }
-    assert UserRest.create(user_body_1) == HTTPStatus.OK, "The POST returned unexpected status code"
-    _, user_1 = UserRest.get(user_body_1["username"])
-    assert UserRest.modify(user_body_1["username"], user_body_2) == HTTPStatus.OK, \
+    assert UserRest.create(input_body) == HTTPStatus.OK, "The POST returned unexpected status code"
+    _, input_user = UserRest.get(input_body["username"])
+    assert UserRest.modify(input_body["username"], modify_body) == HTTPStatus.OK, \
            "The PUT returned unexpected status code"
-    status, user_2 = UserRest.get(user_body_1["username"])
+    status, modify_user = UserRest.get(input_body["username"])
     assert status == HTTPStatus.OK, "The GET returned unexpected status code"
-    user_id = user_2.pop("id")
-    assert user_id == user_1["id"], "Users objects has different id values"
-    assert user_2 != user_body_1, "User after a modification has the same body that it was created"
+    modify_user_id = modify_user.pop("id")
+    assert modify_user == modify_body, "User doesn't have modified body"
+    assert modify_user_id == input_user["id"], "Users objects has different id values"
+    assert modify_user != input_body, "User after a modification has the same body that it was created"
 
