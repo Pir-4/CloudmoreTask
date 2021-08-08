771a5ef3de58385761d947fa659d5c9a7e9f873e
valentin <valentin-42@mail.ru>
2021-08-08 20:23:22 +0300

    start to move next page

--- modules/pages/search_page.py
+++ modules/pages/search_page.py
@@ -1,7 +1,27 @@
 from selenium.webdriver.common.by import By
 from modules.pages.main_page import MainPage
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 class SearchPage(MainPage):
-    LOGO_ID = 'hs-link-module_146731076570910'
+    # NEXT_PAGE_BUTTON = (By.XPATH, '//*[@id="hs_cos_wrapper_module_1530612174918208"]')
+    NEXT_PAGE_BUTTON = (By.CLASS_NAME, 'hs-search-results__next-page')
+    # NEXT_PAGE_BUTTON = (By.CLASS_NAME, 'hs-search-results__pagination')
+    NEXT_PAGE_BUTTON_2 = (By.CLASS_NAME, 'hs-search-results__listing')
+
+    def scroll_down(self):
+        count = 0
+        while True:
+            element = self._get_item(self.NEXT_PAGE_BUTTON_2)
+            js = "arguments[{0}].scrollIntoView();".format(count)
+            self._driver.execute_script(js, element)
+            count +=1
+
+    def pages(self):
+        # self.scroll_down()
+        while True:
+            #t = self._driver.isDisplayes(*self.NEXT_PAGE_BUTTON)
+            next_button = self._get_item(self.NEXT_PAGE_BUTTON_2)
+            actions = ActionChains(self._driver)
+            actions.move_to_element(next_button).perform()
+            self._driver.execute_script("arguments[0].scrollIntoView();", next_button)
 
--- tests/ui_tests/test_ui_main_page.py
+++ tests/ui_tests/test_ui_main_page.py
@@ -12,28 +12,34 @@ def browser():
     driver.quit()
 
 
-def test_verify_top_menu(browser):
-    start_page = MainPage(browser)
-    start_page.load()
-    start_page.close_pop_up_windows()
-    assert start_page.logo(), "Not found logo on the start page"
-    menu_items = start_page.get_menu_items()
-    text_menu_items = [item.text for item in menu_items]
-    for menu_name in UIC.MENU_PAGE_NAMES:
-        assert menu_name in text_menu_items, "Not found menu {} on the start page".format(menu_name)
+@pytest.fixture()
+def start_page(browser):
+    page = MainPage(browser)
+    page.load()
+    page.close_pop_up_windows()
+    return page
 
 
-def test_verify_contact_on_pages(browser):
-    current_page = MainPage(browser)
-    current_page.load()
-    current_page.close_pop_up_windows()
-    assert current_page.logo(), "Not found logo on the start page"
-    for page_name in UIC.MENU_PAGE_NAMES:
-        current_page = current_page.move_menu_item(page_name)
-        assert current_page.logo(), "Not found logo on the {} page".format(page_name)
+# def test_verify_top_menu(start_page):
+#     assert start_page.logo(), "Not found logo on the start page"
+#     menu_items = start_page.get_menu_items()
+#     text_menu_items = [item.text for item in menu_items]
+#     for menu_name in UIC.MENU_PAGE_NAMES:
+#         assert menu_name in text_menu_items, "Not found menu {} on the start page".format(menu_name)
+#
+#
+# def test_verify_contact_on_pages(start_page):
+#     current_page = start_page
+#     assert current_page.logo(), "Not found logo on the start page"
+#     for page_name in UIC.MENU_PAGE_NAMES:
+#         current_page = current_page.move_menu_item(page_name)
+#         assert current_page.logo(), "Not found logo on the {} page".format(page_name)
+#
+#     contact_us_page = current_page.move_menu_item(UIC.MENU_CONTACT_US)
+#     assert contact_us_page.contact_body(), "Contact body doesn't exist on Contact us page"
+#     assert contact_us_page.contact_body_footer(), "Contact body doesn't exist on Contact us page footer"
 
-    contact_us_page = current_page.move_menu_item(UIC.MENU_CONTACT_US)
-    assert contact_us_page.contact_body(), "Contact body doesn't exist on Contact us page"
-    assert contact_us_page.contact_body_footer(), "Contact body doesn't exist on Contact us page footer"
 
-
+def test_verity_search(start_page, keyword="Högset"):
+    search_page = start_page.search(keyword)
+    search_page.pages()
