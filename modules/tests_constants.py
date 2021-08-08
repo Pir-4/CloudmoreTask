bf138befd9643e1f5fc130f1c9e4d567221d1298
valentin <valentin-42@mail.ru>
2021-08-08 17:49:38 +0300

    Separate constants

--- modules/tests_constants.py
+++ modules/tests_constants.py
@@ -1,12 +1,20 @@
-BASE_REST_URL = "https://petstore.swagger.io/v2"
-BASE_UI_URL = "https://web.cloudmore.com/"
+class RestConstants:
+    BASE_REST_URL = "https://petstore.swagger.io/v2"
+    REST_OBJ_USER = "user"
+    USER_CREATE_LIST = "createWithList"
+    PARAM_TYPES = {"id": "int", "username": "name", "firstName": "name",
+                   "lastName": "name", "email": "email", "password": "name",
+                   "phone": "telNumber", "userStatus": "str"}
+    PARAM_LIST = ["id", "username", "firstName", "lastName", "email", "password",
+                  "phone", "userStatus"]
 
-REST_OBJ_USER = "user"
 
-USER_CREATE_LIST = "createWithList"
-
-PARAM_TYPES = {"id": "int", "username": "name", "firstName": "name",
-               "lastName": "name", "email": "email", "password": "name",
-               "phone": "telNumber", "userStatus": "str"}
-PARAM_LIST = ["id", "username", "firstName", "lastName", "email", "password",
-              "phone", "userStatus"]
+class UIConstants:
+    BASE_UI_URL = "https://web.cloudmore.com/"
+    MENU_PLATFORM = "PLATFORM"
+    MENU_SOLUTIONS = "SOLUTIONS"
+    MENU_ABOUT_US = "ABOUT US"
+    MENU_CONTACT_US = "CONTACT US"
+    MENU_BLOG = "BLOG"
+    MENU_CASE_STUDIES = "CASE STUDIES"
+    MENU_PAGE_NAMES = [MENU_PLATFORM, MENU_SOLUTIONS, MENU_ABOUT_US, MENU_CONTACT_US, MENU_BLOG, MENU_CASE_STUDIES]
