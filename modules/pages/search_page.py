4156519bd26fa58e6df9b071dc25cce556463d2e
valentin <valentin-42@mail.ru>
2021-08-09 22:26:50 +0300

    add scroll step

--- modules/pages/search_page.py
+++ modules/pages/search_page.py
@@ -5,10 +5,12 @@ from selenium.common.exceptions import NoSuchElementException, ElementClickInter
 
 class SearchPage(MainPage):
     NEXT_PAGE_BUTTON = (By.CLASS_NAME, 'hs-search-results__next-page')
+    SCROLL_STEP = 50
 
-    def pages(self):
+    def pages(self, wait_count=5000):
         page = self
-        while True:
+        new_height = self.driver.execute_script("return document.body.scrollHeight") / 2
+        for _ in range(wait_count):
             try:
                 if page:
                     yield page
@@ -18,8 +20,8 @@ class SearchPage(MainPage):
             except NoSuchElementException:
                 break
             except ElementClickInterceptedException:
-                new_height = self.driver.execute_script("return document.body.scrollHeight")/2
-                script = "window.scrollTo(0, {0});".format(new_height+5)
+                new_height += self.SCROLL_STEP
+                script = "window.scrollTo(0, {0});".format(new_height)
                 self.driver.execute_script(script)
                 page = None
         return page
